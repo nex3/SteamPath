114098b8cfa139751a4477b81befd67f0cdff5e1
Natalie Weizenbaum <nex342@gmail.com>
2024-08-18 23:40:47 -0700

    Add project files.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # SteamPath
+
+This is a small utility library for finding the location on disk of applications installed with
+Steam. It exposes one function, `SteamPath.Find()`, which takes a Steam app ID (as a string) and
+returns its path, or null if the app isn't installed.
+
+```csharp
+using SteamPath;
+
+var path = SteamPath.Find(1245620);
+Console.WriteLine($"Elden Ring is installed in {path}");
+```
+
+You can look up an application's app ID on <https://steamdb.info/apps/>.
+
+## OS Support
+
+This package currently only supports Windows, because it relies on the Windows registry to figure
+out where Steam itself is installed. I'd be happy to expand this to support other operating systems
+as well if anyone has suggestions for how to locate Steam on them.
---
+++ SteamPath.cs
@@ -0,0 +1,87 @@
+﻿using Gameloop.Vdf;
+using Gameloop.Vdf.Linq;
+using Microsoft.Win32;
+
+namespace SteamPath
+{
+    public class SteamPath
+    {
+        /// <summary>
+        /// A list of all library directories in which the user might have games installed.
+        /// </summary>
+        private static readonly Lazy<List<string>> libraries = new(() =>
+        {
+            var steam = (string?)Registry.GetValue(
+                @"HKEY_CURRENT_USER\Software\Valve\Steam",
+                "SteamPath",
+                null
+            ) ?? throw new SteamPathException("Steam is not installed.");
+            var libraries = new List<string>(new[] { steam });
+
+            VProperty vdf;
+            try
+            {
+                vdf = VdfConvert.Deserialize(
+                    File.ReadAllText($@"{steam}\SteamApps\libraryfolders.vdf"));
+            }
+            catch (IOException e)
+            {
+                throw new SteamPathException(
+                    "Steam installation doesn't contain libraryfolders.vdf.", e);
+            }
+
+            libraries.AddRange(
+                vdf.Value.Cast<VProperty>().Select(child => child.Value["path"]!.Value<string>()));
+            return libraries;
+        });
+
+        /// <param name="appID">
+        /// The numeric ID of the app to find. You can find the ID for a given app on
+        /// https://steamdb.info/apps/.
+        /// </param>
+        /// <returns>
+        /// The path on disk to the Steam application with the given <paramref name="appID"/>, or
+        /// null if no such app is installed.
+        /// </returns>
+        /// <exception cref="SteamPathException">
+        /// If Steam itself is not installed or is somehow corrupted.
+        /// </exception>
+        public static string? Find(string appID)
+        {
+            var manifest = FindManifest(appID);
+            if (manifest == null) return null;
+
+            var vdf = VdfConvert.Deserialize(File.ReadAllText(manifest));
+            var appDir = Path.GetDirectoryName(manifest) + @"\common\" +
+                vdf.Value["installdir"]!.Value<string>();
+            if (!Directory.Exists(appDir)) return null;
+            return appDir;
+        }
+
+        /// <param name="appID">
+        /// The numeric ID of the app to find. You can find the ID for a given app on
+        /// https://steamdb.info/apps/.
+        /// </param>
+        /// <returns>The manifest ACF file for the given <paramref name="appID"/>.</returns>
+        private static string? FindManifest(string appID)
+        {
+            var name = $@"appmanifest_{appID}.acf";
+            foreach (var library in libraries.Value)
+            {
+                var path = $@"{library}\steamapps\{name}";
+                if (File.Exists(path)) return path;
+            }
+
+            return null;
+        }
+
+        /// <summary>An exception thrown when discovering an application's path fails.</summary>
+        public class SteamPathException : Exception
+        {
+            public SteamPathException() : base() { }
+            public SteamPathException(string message) : base(message) { }
+            public SteamPathException(string message, Exception wrapped)
+                : base(message, wrapped) { }
+        }
+    }
+}
